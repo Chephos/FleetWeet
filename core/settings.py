d9b54c4184c12bd8dd123772e1941f9f218b312d
Chephos <efosacharlesabu@gmail.com>
2023-03-11 14:36:41 +0100

    True

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fleetweet-production.up.railway.app', '127.0.0.1']
 # CSRF_TRUSTED_ORIGINS = ['https://fleetweet-production.up.railway.app']
